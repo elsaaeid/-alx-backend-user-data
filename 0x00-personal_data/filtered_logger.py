29fa956352d64fa82edbcb2caa183e7dce36b636
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-02 02:32:06 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,7 +34,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Function that returns a connector to the database
     """
