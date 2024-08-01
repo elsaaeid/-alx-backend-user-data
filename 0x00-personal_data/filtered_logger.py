69583fe890cc72a066bcfeb54fe6aef37b38390e
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-01 19:09:45 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,17 +38,16 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Function that returns a connector to the database
     """
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    dbname = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
 
-    return mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=dbname
-    )
+    db_cn = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return db_cn
 
 
 def main():
