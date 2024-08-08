c9b6bfc12e984b0989856d12ce0141f35c01aff4
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-08 19:14:20 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,6 +64,8 @@ def auth_filter():
         if auth.authorization_header(request) is None\
                 and auth.session_cookie(request) is None:
             abort(401)
+        # if auth.session_cookie(request) is None:
+        #     abort(401)
         if auth.current_user(request) is None:
             abort(403)
 
